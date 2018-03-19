b4565c6b8104d1219c0966133f6facf90fd8db86
Paulo Araújo <paaraujo@gmail.com>
2018-03-19 19:53:52 +0000

    Update Nome - Paulo

--- src/pa/calc/Calculadora.java
+++ src/pa/calc/Calculadora.java
@@ -3,7 +3,7 @@ package pa.calc;
 public class Calculadora {
 
 	public static void main(String[] args) {
-		// OperEx Example
+		// OperEx Example - Paulo
 		OperEx opex=new OperEx();
 		double resEx = opex.oper(1, 2);
 		System.out.println(resEx);
