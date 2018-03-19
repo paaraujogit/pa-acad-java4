184d56f63a8b905e1ab521bec24542f5fd039275
Pedro Negreira <peomiguel@gmail.com>
2018-03-19 22:46:47 +0000

    Update Main Adv. Operations functional

--- src/pa/calc/advoper/advOperMain.java
+++ src/pa/calc/advoper/advOperMain.java
@@ -1,10 +1,17 @@
 package pa.calc.advoper;
 
+
 public class advOperMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		advOperDiv advOperDiv = new advOperDiv();
+        double resDiv = advOperDiv.oper(1, 2, 3);
+        System.out.println(resDiv);
+        
+        advOperMulti advOperMulti = new advOperMulti();
+        double resMulti = advOperMulti.oper(1, 2, 3);
+        System.out.println(resMulti);
 	}
 
 }
